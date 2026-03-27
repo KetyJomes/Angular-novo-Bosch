478b46ac22790148f5a734eabf5df48c5ad3f8ef
KetyJomes <jomessofiaketlyn@gmail.com>
2026-03-27 16:48:13 -0300

    Aula01 Novo Angular - Data - +

--- rplace-api/src/app/app.ts
+++ rplace-api/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
@@ -14,7 +14,25 @@ export class App {
   protected readonly title = signal('rplace-api');
   protected _date = signal(new Date());
 
+  protected _stringDate: Signal<string> = computed(() => {
+    return `${this._date().getFullYear()}-
+    ${this._date().getMonth()}-
+    ${this._date().getDate()}`
+
+  })
+
   ngOnInit(){
     console.log(this.title());
+  }
+
+  addDay(add: boolean = true){
+    this._date.update((oldValue) => {
+      let day = add ? oldValue.getDate()+1 : oldValue.getDate()-1;
+      let month = oldValue.getMonth();
+      let year = oldValue.getFullYear();
+
+      return new Date(year,month,day);
+
+    })
   }
 }
