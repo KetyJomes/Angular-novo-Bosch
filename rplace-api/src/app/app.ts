3c09d7b9e83912d1e2652fa0e41fc33e8c77f0e8
KetyJomes <jomessofiaketlyn@gmail.com>
2026-03-27 16:19:07 -0300

    Aula01 Novo Angular - Data

--- rplace-api/src/app/app.ts
+++ rplace-api/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('rplace-api');
+  protected _date = signal(new Date());
+
+  ngOnInit(){
+    console.log(this.title());
+  }
 }
